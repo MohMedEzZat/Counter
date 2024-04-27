074ca4b8a7d0e239747114065c4eca146a28aa03
Mohamed Ezzat <104213365+MohMedEzZat@users.noreply.github.com>
2024-04-27 22:02:37 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var counter = 0;
+function increment(){
+    counter++;
+    document.getElementById("Ezzat").innerHTML = counter;
+}
+
+var counter = 0;
+function Decrement(){
+    counter--;
+    document.getElementById("Ezzat").innerHTML = counter;
+}
